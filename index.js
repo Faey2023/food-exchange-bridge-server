33c26ca66eae2bd9c9664eedc127fde45243313a
Fahima <sfahima1388@gmail.com>
2023-11-07 16:03:48 +0600

    create data through get method

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+//
 app.get("/", (req, res) => {
   res.send("Food Brand Exchange running.");
 });
@@ -15,11 +21,9 @@ app.listen(port, () => {
 });
 
 //mongodb
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { configDotenv } = require("dotenv");
-const uri =
-  "mongodb+srv://feb_owner:w4qn8LxfCaCdt1zv@cluster0.d9lmwal.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d9lmwal.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,8 +36,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const foodCollection = client.db("febDB").collection("foods");
+
+    app.get("/foods", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -41,7 +53,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
