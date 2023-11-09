67d2cf9d5dd0b855c42ffbae4b49592eba0cd716
Fahima <sfahima1388@gmail.com>
2023-11-09 17:39:17 +0600

    added get operation for myfoods

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://food-exchange-bridge.web.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -61,6 +66,17 @@ async function run() {
       res.send(result);
     });
 
+    //specific
+    app.get("/myfoods", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { donatorEmail: req.query.email };
+      }
+      const result = await foodCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
     //partners
     const partners = client.db("febDB").collection("partners");
 
@@ -83,6 +99,71 @@ async function run() {
       const cursor = foodRequestCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    //delete
+    app.delete("/foodRequest/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodRequestCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //specific
+    app.get("/request", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { requesterEmail: req.query.email };
+      }
+      const result = await foodRequestCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //myfoodrequest
+    const myFoodRequestCollection = client
+      .db("febDB")
+      .collection("myFoodRequest");
+
+    app.post("/myfoodrequest", async (req, res) => {
+      const requestedMyFood = req.body;
+      console.log(req.body);
+      const result = await myFoodRequestCollection.insertOne(requestedMyFood);
+      res.send(result);
+    });
+
+    app.get("/myfoodrequest", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { donatorEmail: req.query.email };
+      }
+      const options = {
+        projection: {
+          requesterEmail: 1,
+          requesterName: 1,
+          requestingDate: 1,
+          status: 1,
+          donatorEmail: 1,
+        },
+      };
+      const result = await myFoodRequestCollection
+        .find(query, options)
+        .toArray();
+      res.send(result);
+    });
+
+    //auth related
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "none",
+          secure: false,
+        })
+        .send({ success: true });
     });
 
     // Connect the client to the server	(optional starting in v4.7)
